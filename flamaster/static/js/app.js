05e5cf9670cd0727c8aa17193f3892b5675f54c8
Yegor Nazarkin <nimnull@gmail.com>
2012-03-17 17:21:41 +0200

    Made client-side with brunch & coffee

---
+++ flamaster/static/js/app.js
@@ -0,0 +1,404 @@
+(function(/*! Brunch !*/) {
+  if (!this.require) {
+    var modules = {}, cache = {}, require = function(name, root) {
+      var module = cache[name], path = expand(root, name), fn;
+      if (module) {
+        return module;
+      } else if (fn = modules[path] || modules[path = expand(path, './index')]) {
+        module = {id: name, exports: {}};
+        try {
+          cache[name] = module.exports;
+          fn(module.exports, function(name) {
+            return require(name, dirname(path));
+          }, module);
+          return cache[name] = module.exports;
+        } catch (err) {
+          delete cache[name];
+          throw err;
+        }
+      } else {
+        throw 'module \'' + name + '\' not found';
+      }
+    }, expand = function(root, name) {
+      var results = [], parts, part;
+      if (/^\.\.?(\/|$)/.test(name)) {
+        parts = [root, name].join('/').split('/');
+      } else {
+        parts = name.split('/');
+      }
+      for (var i = 0, length = parts.length; i < length; i++) {
+        part = parts[i];
+        if (part == '..') {
+          results.pop();
+        } else if (part != '.' && part != '') {
+          results.push(part);
+        }
+      }
+      return results.join('/');
+    }, dirname = function(path) {
+      return path.split('/').slice(0, -1).join('/');
+    };
+    this.require = function(name) {
+      return require(name, '');
+    };
+    this.require.brunch = true;
+    this.require.define = function(bundle) {
+      for (var key in bundle)
+        modules[key] = bundle[key];
+    };
+  }
+}).call(this);(this.require.define({
+  "helpers": function(exports, require, module) {
+    (function() {
+
+  exports.BrunchApplication = (function() {
+
+    function BrunchApplication() {
+      var _this = this;
+      $(function() {
+        _this.initialize(_this);
+        return Backbone.history.start();
+      });
+    }
+
+    BrunchApplication.prototype.initialize = function() {
+      return null;
+    };
+
+    return BrunchApplication;
+
+  })();
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "initialize": function(exports, require, module) {
+    (function() {
+  var BrunchApplication, HomeView, MainRouter,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  BrunchApplication = require('helpers').BrunchApplication;
+
+  MainRouter = require('routers/main_router').MainRouter;
+
+  HomeView = require('views/home_view').HomeView;
+
+  exports.Application = (function(_super) {
+
+    __extends(Application, _super);
+
+    function Application() {
+      Application.__super__.constructor.apply(this, arguments);
+    }
+
+    Application.prototype.initialize = function() {
+      this.router = new MainRouter;
+      return this.homeView = new HomeView;
+    };
+
+    return Application;
+
+  })(BrunchApplication);
+
+  window.app = new exports.Application;
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "routers/main_router": function(exports, require, module) {
+    (function() {
+  var SignupView,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  SignupView = require('views/signup_view').SignupView;
+
+  exports.MainRouter = (function(_super) {
+
+    __extends(MainRouter, _super);
+
+    function MainRouter() {
+      MainRouter.__super__.constructor.apply(this, arguments);
+    }
+
+    MainRouter.prototype.routes = {
+      '': "layout",
+      '/': "layout",
+      '/signup': "signup",
+      '/login': "login"
+    };
+
+    MainRouter.prototype.layout = function() {
+      $('body').html(app.homeView.render());
+      return this.$container = $('#content');
+    };
+
+    MainRouter.prototype.signup = function() {
+      var signupView;
+      this._ensureLayout();
+      signupView = new SignupView;
+      return this.$container.html(signupView.render());
+    };
+
+    MainRouter.prototype.login = function() {
+      this._ensureLayout();
+      return true;
+    };
+
+    MainRouter.prototype._ensureLayout = function() {
+      if ($("home-view") != null) return this.layout();
+    };
+
+    return MainRouter;
+
+  })(Backbone.Router);
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "views/home_view": function(exports, require, module) {
+    (function() {
+  var NavView, SessionModel,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  SessionModel = require('models/session_model').SessionModel;
+
+  NavView = require('views/nav_view').NavView;
+
+  exports.HomeView = (function(_super) {
+
+    __extends(HomeView, _super);
+
+    function HomeView() {
+      HomeView.__super__.constructor.apply(this, arguments);
+    }
+
+    HomeView.prototype.id = 'home-view';
+
+    HomeView.prototype.template = require('./templates/home');
+
+    HomeView.prototype.initialize = function() {
+      return this.navView = new NavView({
+        model: {
+          index: ["Index", '/'],
+          signup: ["Sign Up", '/signup'],
+          login: ["Login", '/login']
+        },
+        router: app.router,
+        session: this.getCurrentUser()
+      });
+    };
+
+    HomeView.prototype.render = function() {
+      this.$el.html(this.template);
+      this.$el.find("#nav-container").html(this.navView.render());
+      return this.el;
+    };
+
+    HomeView.prototype.getCurrentUser = function() {
+      this.session = new SessionModel;
+      this.session.fetch();
+      return this.session;
+    };
+
+    return HomeView;
+
+  })(Backbone.View);
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "models/session_model": function(exports, require, module) {
+    (function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  exports.SessionModel = (function(_super) {
+
+    __extends(SessionModel, _super);
+
+    function SessionModel() {
+      SessionModel.__super__.constructor.apply(this, arguments);
+    }
+
+    SessionModel.prototype.urlRoot = '/account/sessions/';
+
+    SessionModel.prototype.emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    SessionModel.prototype.validate = function(attrs) {
+      var response;
+      response = {
+        status: 'success'
+      };
+      if (!this.emailRegex.test(attrs.email)) {
+        response.status = 'failed';
+        response.email = 'This is not valid email address';
+      }
+      if (attrs.password !== attrs.confirm) {
+        response.status = 'failed';
+        response.confirm = "Confirmation don't match";
+      }
+      if (response.status === 'failed') return response;
+    };
+
+    SessionModel.prototype.is_anonymous = function() {
+      return console.log(this.toJSON());
+    };
+
+    return SessionModel;
+
+  })(Backbone.Model);
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "views/nav_view": function(exports, require, module) {
+    (function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  exports.NavView = (function(_super) {
+
+    __extends(NavView, _super);
+
+    function NavView() {
+      NavView.__super__.constructor.apply(this, arguments);
+    }
+
+    NavView.prototype.tagName = 'ul';
+
+    NavView.prototype.className = "nav nav-pills";
+
+    NavView.prototype.template = require('./templates/nav');
+
+    NavView.prototype.initialize = function(options) {
+      var router,
+        _this = this;
+      router = options.router;
+      router.on("route:layout", function() {
+        _this.$el.find("li").removeClass('active');
+        return _this.$el.find(".n-index").addClass('active');
+      });
+      router.on("route:login", function() {
+        _this.$el.find("li").removeClass('active');
+        return _this.$el.find(".n-login").addClass('active');
+      });
+      return router.on("route:signup", function() {
+        _this.$el.find("li").removeClass('active');
+        return _this.$el.find(".n-signup").addClass('active');
+      });
+    };
+
+    NavView.prototype.render = function() {
+      this.$el.html(this.template({
+        routes: this.model
+      }));
+      return this.el;
+    };
+
+    return NavView;
+
+  })(Backbone.View);
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
+  "views/signup_view": function(exports, require, module) {
+    (function() {
+  var SessionModel,
+    __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  SessionModel = require('models/session_model').SessionModel;
+
+  exports.SignupView = (function(_super) {
+
+    __extends(SignupView, _super);
+
+    function SignupView() {
+      SignupView.__super__.constructor.apply(this, arguments);
+    }
+
+    SignupView.prototype.template = require('./templates/signup');
+
+    SignupView.prototype.events = {
+      "click #signup-form button[type='submit']": "submit"
+    };
+
+    SignupView.prototype.initialize = function() {
+      return this.$el.find("#signup-form").submit(function() {
+        console.log(arguments);
+        return false;
+      });
+    };
+
+    SignupView.prototype.serializeForm = function(form) {
+      var array, attr, response, _i, _len, _ref;
+      _ref = [form.serializeArray(), {}], array = _ref[0], response = _ref[1];
+      for (_i = 0, _len = array.length; _i < _len; _i++) {
+        attr = array[_i];
+        response[attr.name] = attr.value;
+      }
+      return response;
+    };
+
+    SignupView.prototype.render = function() {
+      this.$el.html(this.template());
+      return this.el;
+    };
+
+    SignupView.prototype.submit = function(ev) {
+      var $form, formData, session,
+        _this = this;
+      $form = $(ev.target).parents('form');
+      this.clearErrors();
+      formData = this.serializeForm($form);
+      session = new SessionModel(formData);
+      session.on('error', function(session, error) {
+        var field, message, _results;
+        _results = [];
+        for (field in error) {
+          message = error[field];
+          _results.push(_this.renderError(field, message));
+        }
+        return _results;
+      });
+      session.save();
+      return false;
+    };
+
+    SignupView.prototype.renderError = function(field, message) {
+      var $el, error;
+      $el = this.$el.find("form input[name='" + field + "']");
+      $el.parents(".control-group").addClass('error');
+      error = $(document.createElement('span')).addClass('help-inline error').text(message);
+      return $el.after(error);
+    };
+
+    SignupView.prototype.clearErrors = function() {
+      this.$el.find(".control-group").removeClass('error');
+      return this.$el.find("span.help-inline.error").remove();
+    };
+
+    return SignupView;
+
+  })(Backbone.View);
+
+}).call(this);
+
+  }
+}));
---
+++ flamaster/static/js/templates.js
@@ -0,0 +1,221 @@
+(this.require.define({
+  "views/templates/home": function(exports, require, module) {
+    module.exports = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+    
+      __out.push('<div class="container">\n  <div class="row">\n    <div class="span12" id="nav-container"></div>\n  </div>\n  <div class="row">\n    <div class="span12" id="content">\n      <div class="hero-unit">\n        <h1>Findevent</h1>\n        <p>Work in progress…</p>\n        <p>\n          <a href="/#/signup" class="btn btn-primary btn-large">\n            Sign Up\n          </a>\n          or\n          <a href="/#/login" class="btn btn-success btn-large">\n            Login\n          </a>\n        </p>\n      </div>\n    </div>\n  </div>\n</div>\n');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+  }
+}));
+(this.require.define({
+  "views/templates/nav": function(exports, require, module) {
+    module.exports = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+      var path, route, _ref;
+    
+      _ref = this.routes;
+      for (route in _ref) {
+        path = _ref[route];
+        __out.push('\n<li class="n-');
+        __out.push(__sanitize(route));
+        __out.push('"><a href="/#');
+        __out.push(__sanitize(path[1]));
+        __out.push('">');
+        __out.push(__sanitize(path[0]));
+        __out.push('</a></li>\n');
+      }
+    
+      __out.push('\n');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+  }
+}));
+(this.require.define({
+  "views/templates/signup": function(exports, require, module) {
+    module.exports = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+    
+      __out.push('<div class="container">\n  <div class="row">\n    <div class="span12"><h2>Sign up please</h2></div>\n  </div>\n  <div class="row">\n    <div class="span12">\n      <form class="form-horizontal" id="signup-form">\n        <fieldset>\n          <legend>Enter your credetials to create an account</legend>\n          <div class="control-group">\n            <label class="control-label" for="email">Email</label>\n            <div class="controls">\n              <input type="text" name="email" class="input-large" placeholder="email will be used as login"/>\n            </div>\n          </div>\n          <div class="form-actions">\n            <button type="submit" class="btn btn-primary">Sign Up</button>\n            <button class="btn">Cancel</button>\n          </div>\n        </fieldset>\n      </form>\n    </div>\n  </div>\n</div>\n');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+  }
+}));
+(this.require.define({
+  "views/templates/login": function(exports, require, module) {
+    module.exports = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+    
+      __out.push('<h2>Not implemented yet</h2>\n');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+  }
+}));
