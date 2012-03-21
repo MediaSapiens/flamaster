f71a03b5d6f18b1f72f9e4b22baba5135e7c2bf4
Yegor Nazarkin <nimnull@gmail.com>
2012-03-21 15:58:07 +0200

    Generalized test context invocation

--- tests/test_account_api.py
+++ tests/test_account_api.py
@@ -2,39 +2,54 @@ from flask import url_for
 from flask.helpers import json
 from flamaster.app import app, db
 
+from functools import wraps
+
+
+def request_context(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with app.test_request_context('/'):
+            return func(*args, **kwargs)
+    return wrapper
+
 
 def setup_module(module):
-    print(app)
     db.create_all()
 
 
-def test_flask_invocation(flaskapp):
-    with flaskapp.test_client() as c:
+def test_flask_invocation():
+    with app.test_client() as c:
         resp = c.get('/')
         assert 'title' in resp.data
 
 
-def test_account_api_invocation(flaskapp):
-    with flaskapp.test_request_context('/'):
-        sessions_url = url_for('account.sessions')
-        c = flaskapp.test_client()
-        resp = c.get(sessions_url)
-        assert resp.content_type == 'application/json'
+@request_context
+def test_account_api_invocation():
+    sessions_url = url_for('account.sessions')
+    c = app.test_client()
+    resp = c.get(sessions_url)
+    assert resp.content_type == 'application/json'
 
-        json_response = json.loads(resp.data)
-        assert json_response['object']['is_anonymous'] == True
+    json_response = json.loads(resp.data)
+    assert json_response['object']['is_anonymous'] == True
 
 
-def test_session_creation(flaskapp):
-    with flaskapp.test_request_context('/'):
-        sessions_url = url_for('account.sessions')
-        with flaskapp.test_client() as c:
-            resp = c.post(sessions_url,
-                    data=json.dumps({'email': 'test@mail.com'}),
-                    content_type='application/json')
-            j_resp = json.loads(resp.data)
-            assert isinstance(j_resp['object']['uid'], int)
-            assert j_resp['object']['is_anonymous'] == False
+@request_context
+def test_session_creation():
+    sessions_url = url_for('account.sessions')
+    with app.test_client() as c:
+        resp = c.post(sessions_url,
+                data=json.dumps({'email': 'test@mail.com'}),
+                content_type='application/json')
+        j_resp = json.loads(resp.data)
+        assert isinstance(j_resp['object']['uid'], int)
+        assert j_resp['object']['is_anonymous'] == False
+
+
+@request_context
+def test_authorization():
+    pass
 
 
 def teardown_module(module):
