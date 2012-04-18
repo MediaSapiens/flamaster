02a2840874201722cb483ad12537be3ee6996751
Andrey Doroshenko <dorosshh@gmail.com>
2012-04-18 16:10:58 +0300

    add trafaret validetion in password_confirm

--- flamaster/account/views.py
+++ flamaster/account/views.py
@@ -1,9 +1,10 @@
 from flask import flash, request, redirect, render_template, url_for, abort
 
-from flamaster.core.utils import send_email
+from flamaster.core.utils import send_email, change_password_validate
 
 from . import account
 from .models import User
+import trafaret as t
 
 __all__ = ['request_reset', 'confirm_reset']
 
@@ -27,8 +28,12 @@ def request_reset():
 @account.route('/reset/<token>/', methods=['GET', 'POST'])
 def confirm_reset(token):
     user = User.validate_token(token) or abort(403)
-    password = request.form.get('password', None)
-    password_confirm = request.form.get('password_confirm', None)
-    if password and password_confirm and password == password_confirm:
-        user.set_password(password)
+    if len(request.form):
+        d = t.Dict(password=t.String, password_confirm=t.String).append(
+            change_password_validate)
+        d.check({'password': request.form.get('password', None),
+                 'password_confirm': request.form.get(
+                     'password_confirm', None)})
+        user.set_password(request.form.get('password'))
+        user.save()
     return render_template('password_reset_confirm.html', token=token)
