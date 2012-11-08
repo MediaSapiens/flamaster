629f8177f9e0a39ba5f60d664284fe2ef61458b4
I159 <pekelny@gmail.com>
2012-11-08 17:38:52 +0200

    Abstract class

--- payment/methods/base.py
+++ payment/methods/base.py
@@ -1,2 +1,32 @@
 class BasePaymentMethod(object):
-    pass
+    """ Base API handler class, established with respect to the PayPal API.
+    """
+    _endpoint = ''
+    _cancel_url = ''
+    _return_url = ''
+    _format = ''
+
+    def call(self):
+        """ Provide all call elements and meke request
+        """
+        raise NotImplementedError
+
+    def paylod(self):
+        """ Provide call payloads
+        """
+        raise NotImplementedError
+
+    def method(self, case):
+        """ The API operation you are addressing
+        """
+        raise NotImplementedError
+
+    def amount(self, total):
+        """ Calculate payment cash amount
+        """
+        raise NotImplementedError
+
+    def headers(self):
+        """ Provide request headers
+        """
+        raise NotImplementedError
