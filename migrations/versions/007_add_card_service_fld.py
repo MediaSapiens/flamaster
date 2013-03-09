fac8c961b7c0d4b778a480bca4840411f2c09fb1
Alex Isayko <alex.isayko@gmail.com>
2013-03-09 13:28:53 +0200

    Fixed migration for cart service field

--- migrations/versions/007_add_card_service_fld.py
+++ migrations/versions/007_add_card_service_fld.py
@@ -4,12 +4,12 @@ from migrate import *
 
 def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
-    cart = Table('cart', meta, autoload=True)
+    cart = Table('carts', meta, autoload=True)
     service = Column('service', String(), default='')
     service.create(cart)
 
 
 def downgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
-    cart = Table('cart', meta, autoload=True)
+    cart = Table('carts', meta, autoload=True)
     cart.c.service.drop()
