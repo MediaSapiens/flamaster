56f4ac0891b598f9d7fcedeac05411035bd9d282
Yehor Nazarkin <nimnull@gmail.com>
2013-03-22 15:00:11 +0200

    Moved propertized price_options to the base class

--- product/documents.py
+++ product/documents.py
@@ -42,7 +42,15 @@ class BaseProductVariant(Document, DocumentMixin):
         'collection': 'product_variants'
     }
 
-    price_options = ListField(EmbeddedDocumentField(BasePriceOption))
+    _price_options = ListField(EmbeddedDocumentField(BasePriceOption))
+
+    def _get_price_options(self):
+        return self._price_options
+
+    def _set_price_options(self, value):
+        self._price_options = map(BasePriceOption.convert, value)
+
+    price_options = property(_get_price_options, _set_price_options)
 
     def __get_prices(self):
         prices = [Decimal(0)]
