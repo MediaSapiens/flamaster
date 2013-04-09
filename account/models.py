4b110944d50574661dc5eaced997ecbc3dd3ded0
Yehor Nazarkin <nimnull@gmail.com>
2013-04-09 18:19:46 +0300

    Ommited fragile data in serializer

--- account/models.py
+++ account/models.py
@@ -245,10 +245,11 @@ class User(db.Model, CRUDMixin, UserMixin):
 
     def as_dict(self, include=None, exclude=None):
         include, exclude = exclude or [], include or []
-        exclude.append('password')
-        include.extend(["first_name", "last_name", "phone", "billing_address",
-                        "is_superuser"])
+        exclude.extend(['password', 'remember_token', 'authentication_token'])
+        include.extend(['first_name', 'last_name', 'phone', 'billing_address',
+                        'is_superuser'])
         return super(User, self).as_dict(include, exclude)
+
 
 class BankAccount(db.Model, CRUDMixin):
     bank_name = db.Column(db.Unicode(512))
