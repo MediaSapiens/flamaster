7b59d53c9b8b240d174a46e3583f793e05e4cab9
Yehor Nazarkin <nimnull@gmail.com>
2013-04-30 15:07:24 +0300

    Added test launcher for nosetests

--- core/commands.py
+++ core/commands.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
+import nose
+
 from flask.ext.script import Command
 from flamaster.extensions import db
+from tests.initial_data import Requirements
 
 __all__ = ['CreateAll', 'DropAll']
 
@@ -15,3 +18,20 @@ class DropAll(Command):
 
     def run(self):
         db.drop_all()
+
+
+class RunTests(Command):
+
+    def handle(self, app, *args, **kwargs):
+        """
+        Handles the command with given app. Default behaviour is to call within
+        a test request context.
+        """
+        app.config.from_object('test_settings')
+        with app.app_context():
+            db.drop_all(app=app)
+            db.create_all(app=app)
+            reqs = Requirements()
+            reqs('user')
+            reqs('categories')
+            nose.run(argv=['-xs', 'tests'])
