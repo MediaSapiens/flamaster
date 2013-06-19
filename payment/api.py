587abbd19bb9962b40848e073be739194c787788
Maxim Starodubcev <maxim@trambroid.com>
2013-06-19 11:53:36 +0300

    Refactoring methods

--- payment/api.py
+++ payment/api.py
@@ -1,26 +1,23 @@
 from flamaster.core.resources import Resource
-from flamaster.core.decorators import api_resource, method_wrapper
+from flamaster.core.decorators import api_resource
 from flamaster.core import http
 from flamaster.core.utils import jsonify_status_code
 
-from flask import current_app
+from flask import current_app, abort
 from . import payment as bp
 
 
 @api_resource(bp, 'settings', {'short': None})
 class SettingsResource(Resource):
 
-    @method_wrapper(http.METHOD_NOT_ALLOWED)
     def put(self, **kwargs):
-        return ''
+        abort(http.METHOD_NOT_ALLOWED)
 
-    @method_wrapper(http.METHOD_NOT_ALLOWED)
     def post(self, **kwargs):
-        return ''
+        abort(http.METHOD_NOT_ALLOWED)
 
-    @method_wrapper(http.METHOD_NOT_ALLOWED)
     def delete(self, **kwargs):
-        return ''
+        abort(http.METHOD_NOT_ALLOWED)
 
     def get(self, **kwargs):
         settings = current_app.config['PAYMENT_METHODS']
