aceff720d8eb05576968fa954e8bfe483e88e1b3
Maxim Starodubcev <maxim@trambroid.com>
2013-06-27 17:53:52 +0300

    Fix paymegit status

--- product/datastore.py
+++ product/datastore.py
@@ -46,7 +46,7 @@ class OrderDatastore(AbstractDatastore):
 
         kwargs.update({
             'reference': str(uuid.uuid4().node),
-            'delivery_method': 'provider',
+            'delivery_method': 'DHL',
             'customer': customer,
             'goods_price': goods_price,
             'payment_fee': payment_fee,
