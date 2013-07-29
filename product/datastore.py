43bf516fc1d9cafd3b5c90bbfaa7fbfd6a0c47f4
Maxim Starodubcev <maxim@trambroid.com>
2013-07-29 12:34:52 +0300

    Hot fix

--- product/datastore.py
+++ product/datastore.py
@@ -56,7 +56,7 @@ class OrderDatastore(AbstractDatastore):
             max_discount = items[-1]
             return (self.__get_discount(max_discount), max_discount[2])
 
-        return (None, False)
+        return (0, False)
 
     def get_cart_discount(self, goods_price=0):
         now = datetime.now()
