5c0cce28c7accacabc87651ffc1049f1ea37d4a1
metametadata <yuri.govor@gmail.com>
2013-08-27 18:35:45 +0300

    fixed message

--- delivery/methods/base.py
+++ delivery/methods/base.py
@@ -3,4 +3,4 @@
 class BaseDelivery(object):
 
     def check_availability(self, customer, order):
-        raise NotImplementedError("This method not implemented")
+        raise NotImplementedError("This method is not implemented")
