c2e417d1d8b8446838bdbc3729711642ef78d313
metametadata <yuri.govor@gmail.com>
2013-08-27 18:37:57 +0300

    removed unused code

--- delivery/api.py
+++ delivery/api.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import
-from flask import current_app
-
 from flamaster.core.decorators import api_resource
 from flamaster.core.resources import Resource
-from flamaster.core.utils import jsonify_status_code
-
 from werkzeug import import_string
 from . import bp
 
@@ -13,13 +9,7 @@ from . import bp
 class DeliveryResource(Resource):
 
     def get(self, id=None):
-        resolved_options = {}
-        for key, value in current_app.config['DELIVERY_OPTIONS'].iteritems():
-            if 'default' not in value:
-                resolved_options[key] = import_string(value['module'])
-
-        items = [{key: Cls.name} for key, Cls in resolved_options.iteritems()]
-        return self.__build_response(items)
+        pass
 
     def put(self, id=None):
         raise NotImplementedError()
@@ -29,10 +19,3 @@ class DeliveryResource(Resource):
 
     def delete(self, id=None):
         raise NotImplementedError()
-
-    def __build_response(self, items):
-        response = {
-            'objects': items,
-            'meta': None
-        }
-        return jsonify_status_code(response)
