c250c6bcadc0d4605d5982fd882b79cedf78e6ce
Maxim Starodubcev <maxim@trambroid.com>
2013-08-28 15:20:55 +0300

    Fux checkout with cash payment

--- payment/methods/cash.py
+++ payment/methods/cash.py
@@ -1,5 +1,10 @@
 from .base import BasePaymentMethod
+from flamaster.product.models import PaymentTransaction
 
 
 class CashPaymentMethod(BasePaymentMethod):
     method_name = 'cash'
+
+    def process_payment(self, **kwargs):
+        return PaymentTransaction.create(status=PaymentTransaction.ACCEPTED,
+                                details='cash: %s' % self.order_data.__str__())
