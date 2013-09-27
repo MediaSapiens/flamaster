56d97dbac9299b68862437081511afbdc59770b6
metametadata <yuri.govor@gmail.com>
2013-09-27 12:56:34 +0300

    get_locale() now uses X-Client-Locale header if present

--- factory.py
+++ factory.py
@@ -160,6 +160,10 @@ def show_page_not_found(error):
 
 def get_locale(app):
     def closure():
+        client_locale = request.headers.get('X-Client-Locale')
+        if client_locale:
+            return client_locale
+
         languages = app.config['ACCEPT_LANGUAGES']
         matched = request.accept_languages.best_match(languages)
         language = session.get(app.config['LOCALE_KEY'], matched)
